db181bb8b37d4653e5803aaf0c7e1d0cd7b1170d
Ana Eusebio <ana.eusebio@SUMUP-C02ZL0MKL.local>
2021-02-02 10:48:13 -0300

    listas, utilizacao do for recebendo o valor e fazer interacao entre elas

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,8 +1,17 @@
 defmodule Cards do
-
   def create_deck do
-    ["Ace", "Two", "Three"]
+      values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    cards = for value <- values do
+    for suit <- suits do
+      "#{value} of #{suit}"
+    end
   end
+
+  List.flatten(cards)
+  end
+
 
   def shuffle(deck) do
     Enum.shuffle(deck)
@@ -10,6 +19,10 @@ defmodule Cards do
 
   def contains?(deck, card) do
     Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
   end
 
 end
