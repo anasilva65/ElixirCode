3fe1f9b06a9e415722738f58e5c219114c617e35
Ana Eusebio <ana.eusebio@SUMUP-C02ZL0MKL.local>
2021-02-03 17:48:20 -0300

    introd ao pipe operator, ao usar ele nao é nec fzr nenhuma atrib a uma var tmporaria

--- lib/cards.ex
+++ lib/cards.ex
@@ -39,4 +39,17 @@ defmodule Cards do
     end
 
   end
+
+  def create_hand(hand_size) do
+    deck = Cards.create_deck
+    deck = Cards.shuffle(deck)
+    hand = Cards.deal(deck, hand_size)
+  end
+
+  # Now using the pipe operator instead
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+  end
 end
