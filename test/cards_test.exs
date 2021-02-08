87056a15897cd3e1ef1ffccb5adbfe899174f069
Ana Eusebio <ana.eusebio@SUMUP-C02ZL0MKL.local>
2021-02-08 18:15:09 -0300

    primeiros testes

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,9 +2,14 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "espero que um mais um seja diferente de tres" do
 
-    assert Cards.shuffle(deck) == ["Ace", "Two", "Three"]
+    assert 1 + 1 != 3
   end
+
+  test "espero que um mais um seja igual a dois" do
+    assert 1 + 1 == 2
+  end
+
+
 end
