9c32b7a779c51a24f2eadf66415bc6250a07f857
anasilva65 <annadasilva89@hotmail.com>
2021-02-09 14:11:28 -0300

    teste para verificar se o baralho faz 20 cards

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -11,5 +11,10 @@ defmodule CardsTest do
     assert 1 + 1 == 2
   end
 
+  test "ter a certeza que um deck cria 20 cards diferentes" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 20
+  end
+
 
 end
