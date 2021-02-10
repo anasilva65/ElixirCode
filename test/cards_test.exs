dd808c91e957c0bb02b69e613101a889580c3c7f
anasilva65 <annadasilva89@hotmail.com>
2021-02-10 10:10:25 -0300

    teste para verificar se o deck está embaralhando

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,19 +2,14 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "espero que um mais um seja diferente de tres" do
-
-    assert 1 + 1 != 3
-  end
-
-  test "espero que um mais um seja igual a dois" do
-    assert 1 + 1 == 2
-  end
-
   test "ter a certeza que um deck cria 20 cards diferentes" do
     deck_length = length(Cards.create_deck)
     assert deck_length == 20
   end
 
+  test "verificar se realmente o deck está sendo embaralhado aleatoriamente" do
+    deck = Cards.create_deck
+    assert deck != Cards.shuffle(deck)
+  end
 
 end
